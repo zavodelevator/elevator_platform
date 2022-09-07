dc4cbfb60f84b90710be58a2dd8e04428af3a591
zavodelevator <68054345+zavodelevator@users.noreply.github.com>
2022-09-07 14:49:45 +0300

    heroku_komit

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
+# and maximum, this matches the default thread size of Active Record.
 #
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
@@ -13,7 +13,7 @@ threads min_threads_count, max_threads_count
 #
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
 port ENV.fetch("PORT") { 3000 }
 
