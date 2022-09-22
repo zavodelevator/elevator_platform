117e9450a6b06ae4453a044e3969dbe8f8a12675
zavodelevator <68054345+zavodelevator@users.noreply.github.com>
2022-09-22 16:43:23 +0300

    add_divase
    add divase

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
   resources :contracts
   resources :workers
   resources :banks
@@ -10,5 +12,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
